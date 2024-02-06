feb77f2dd97ca8ccffad0ad174d634ebf340696b
Lukács Péter <peter.lukacs16@gmail.com>
2024-02-06 22:42:20 +0100

    Adding new Task with data validation

--- database/migrations/2024_02_05_204524_create_tasks_table.php
+++ database/migrations/2024_02_05_204524_create_tasks_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('title');
             $table->text('description');
             $table->text('long_description')->nullable();
-            $table->boolean('completed');
+            $table->boolean('completed')->default(false);
 
             $table->timestamps();
         });
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Task;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
@@ -21,7 +22,7 @@ Route::get('/', function () {
 
 Route::get('/tasks', function () {
     return view('index', [
-        'tasks' => \App\Models\Task::latest()->get()
+        'tasks' => Task::latest()->get()
     ]);
 })->name('tasks.index');
 
@@ -30,14 +31,26 @@ Route::view('/tasks/create', 'create')
 
 Route::get('/tasks/{id}', function ($id) {
     return view('show', [
-        'task' => \App\Models\Task::findOrFail($id)]);
+        'task' => Task::findOrFail($id)]);
 })->name('tasks.show');
 
 Route::post('/tasks', function(Request $request){
-    dd($request->all());
+    $data = $request->validate([
+        'title' => 'required|max:255',
+        'description' => 'required',
+        'long_description' => 'required',
+        ]);
+
+    $task = new Task;
+    $task->title = $data['title'];
+    $task->description = $data['description'];
+    $task->long_description = $data['long_description'];
+    $task->save();
+
+    return redirect()->route('tasks.show', ['id' => $task->id]);
 })->name('tasks.store');
 
-/*Route::get('/xxx', function () {
+/*Route::get('/xx', function () {
     return 'Hello';
 })->name('hello');
 
@@ -52,8 +65,3 @@ Route::get('/greet/{name}', function($name){
 Route::fallback(function (){
     return 'Still got somewhere!';
 });
-
-// GET
-// POST
-// PUT
-// DELETE
