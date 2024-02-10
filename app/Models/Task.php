0ed10dc61ebbe6300d66e735cc41f095a68d1542
Lukács Péter <peter.lukacs16@gmail.com>
2024-02-10 01:46:09 +0100

    Added Route Model Binding, Form Requests, Mass Assignment, deleting data, reusable blade.php for creating and editing tasks

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -5,12 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-/**
- * @method static findOrFail($id)
- * @method static latest()
- * @property mixed $id
- */
 class Task extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['title', 'description', 'long_description'];
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Requests\TaskRequest;
 use App\Models\Task;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -28,61 +29,53 @@ Route::get('/tasks', function () {
 Route::view('/tasks/create', 'create')
         ->name('tasks.create');
 
-Route::get('/tasks/{id}/edit', function ($id) {
+Route::get('/tasks/{task}/edit', function (Task $task) {
     return view('edit', [
-        'task' => Task::findOrFail($id)]);
+        'task' => $task
+        ]);
 })->name('tasks.edit');
 
-Route::get('/tasks/{id}', function ($id) {
+Route::get('/tasks/{task}', function (Task $task) {
     return view('show', [
-        'task' => Task::findOrFail($id)]);
+        'task' => $task
+    ]);
 })->name('tasks.show');
 
-Route::post('/tasks', function(Request $request){
-    $data = $request->validate([
-        'title' => 'required|max:255',
-        'description' => 'required',
-        'long_description' => 'required',
-        ]);
+Route::post('/tasks', function(TaskRequest $request){
+//    $data= ;
+//    $task = new Task;
+//    $task->title = $data['title'];
+//    $task->description = $data['description'];
+//    $task->long_description = $data['long_description'];
+//    $task->save();
 
-    $task = new Task;
-    $task->title = $data['title'];
-    $task->description = $data['description'];
-    $task->long_description = $data['long_description'];
-    $task->save();
+    // Mass Assignment
+    $task = Task::create($request->validated());
 
-    return redirect()->route('tasks.show', ['id' => $task->id])
+    return redirect()->route('tasks.show', ['task' => $task->id])
         ->with('success', 'Task created successfully!');
 })->name('tasks.store');
 
-Route::put('/tasks/{id}', function($id, Request $request){
-    $data = $request->validate([
-        'title' => 'required|max:255',
-        'description' => 'required',
-        'long_description' => 'required',
-    ]);
+Route::put('/tasks/{task}', function(Task $task, TaskRequest $request){
+    $data=
+//    $task->title = $data['title'];
+//    $task->description = $data['description'];
+//    $task->long_description = $data['long_description'];
+//    $task->save();
 
-    $task = Task::findOrFail($id);
-    $task->title = $data['title'];
-    $task->description = $data['description'];
-    $task->long_description = $data['long_description'];
-    $task->save();
+    //Mass Assignment
+    $task->update($request->validated());
 
-    return redirect()->route('tasks.show', ['id' => $task->id])
+    return redirect()->route('tasks.show', ['task' => $task->id])
         ->with('success', 'Task edited successfully!');
 })->name('tasks.update');
 
-/*Route::get('/xx', function () {
-    return 'Hello';
-})->name('hello');
+Route::delete('/tasks/{task}', function(Task $task){
+    $task->delete();
 
-Route::get('/hallo', function() {
-    return redirect()->route('hello');
-});
-
-Route::get('/greet/{name}', function($name){
-    return 'Hello ' . $name . '!';
-});*/
+    return redirect()->route('tasks.index')
+        ->with('success', 'Task deleted successfully!');
+})->name('tasks.destroy');
 
 Route::fallback(function (){
     return 'Still got somewhere!';
